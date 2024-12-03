fd96f9880e8d2cdb591c369d76fecade6965d4a9
traviszmotony <traviszmotony2@gmail.com>
2024-12-04 00:00:00 +0500

    feat: update forms

---
+++ config/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.models import User
+from insights.models import UserProfile
+
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    # role = forms.ChoiceField(choices=UserProfile.ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class CustomLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'style': 'color: black;',
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'style': 'color: black;',
+        })
+    )
+
+# touched on 2025-05-27T15:28:51.111952Z
---
+++ insights/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Client, Assessment, PillarScore, Insight, Recommendation, UserProfile, Quote, Match, DimensionScore, ChangeMilestone
+
+admin.site.register(Client)
+admin.site.register(Assessment)
+admin.site.register(PillarScore)
+admin.site.register(Insight)
+admin.site.register(Recommendation)
+admin.site.register(UserProfile)
+admin.site.register(Quote)
+admin.site.register(Match)
+admin.site.register(DimensionScore)
+admin.site.register(ChangeMilestone)
+
+# touched on 2025-05-27T15:28:51.113874Z
